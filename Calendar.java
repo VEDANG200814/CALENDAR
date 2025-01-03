de96983ff21055c4c9d64b8ee807ac685256bc7d
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2025-01-04 00:08:45 +0530

    Update Calendar.java
    In this update you will get the months seperately.

--- Calendar.java
+++ Calendar.java
@@ -8,38 +8,47 @@ public class Calendar {
             else
             mm+=365;
         }
-        l=(mm+1)%7;
+        l=(mm+1);
         return l;
+    }
+    int leap(int y,int i){
+        int b;
+        if((i<=7&&i%2==1)||(i>=8&&i%2==0))
+        b=31;
+        else if(((y%4==0&&y%100!=0)||(y%400==0))&&i==2)
+        b=29;
+        else if(i==2)
+        b=28;
+        else
+        b=30;
+        return b;
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         Calendar ob=new Calendar();
         System.out.println("Enter year");
         int y=sc.nextInt(),i,j,k,l,b=0;
-        l=ob.start(y)+1;
+        l=ob.start(y);
+        System.out.println("Enter the month number");
+        int month=sc.nextInt();
         String m[]={"JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"};
-        for(i=1;i<=12;i++){
-            if((i<=7&&i%2==1)||(i>=8&&i%2==0))
-            b=31;
-            else if(((y%4==0&&y%100!=0)||(y%400==0))&&i==2)
-            b=29;
-            else if(i==2)
-            b=28;
-            else
-            b=30;
-            System.out.println(m[i-1]+": ");
-            System.out.println("S\tM\tT\tW\tT\tF\tS");
-            for(k=1;k<l;k++)
-            System.out.print(" \t");
-            for(j=1;j<=b;j++,k++){
-                System.out.print(j+"\t");
-                l++;
-                if(l==8||k%7==0){
-                    System.out.println();
-                    l=1;
-                }
+        System.out.println(m[month-1]);
+        System.out.println("S\tM\tT\tW\tT\tF\tS");
+        for(i=1;i<month;i++){
+            b=ob.leap(y,i);
+            l+=b;
+        }
+        b=ob.leap(y, i);
+        l=l%7+1;
+        for(j=1;j<l;j++)
+        System.out.print(" \t");
+        for(k=1;k<=b;k++,j++){
+            System.out.print(k+"\t");
+            l++;
+            if(l==8||j%7==0){
+                System.out.println();
+                l=1;
             }
-            System.out.println();
         }
     }
 }
